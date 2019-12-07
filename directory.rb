514408d0cb21038defb30b6d662bec4ceb47ce2f
Shaun <shaunlowshuennyuh@hotmail.com>
2019-12-07 23:34:23 +0000

    Directory.rb Step13

--- directory.rb
+++ directory.rb
@@ -19,7 +19,7 @@ students = [
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
       @students << {name: name, cohort: :november}
       puts "Now we have #{@students.count} students"
@@ -68,7 +68,6 @@ def process(selection)
       save_students
     when "4"
       load_students
-      
     when "9"
       exit
     else
@@ -79,7 +78,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -95,13 +94,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close  
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exists."
+    exit
+  end
 end
 
 interactive_menu
